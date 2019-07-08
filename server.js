da596dd083440bcd60af0d251a7e07a7c6195e01
jaydonhashimoto <jaydonhashi@gmail.com>
2019-07-08 11:18:42 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+    if (req.file === null) {
+        return res.status(400).json({ msg: 'No file uploaded' });
+    }
+    //get file
+    const file = req.files.file;
+
+    //move file to directory
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        //if there is an error, send 500 with error
+        if (err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        //else send 200 with file name and path
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    })
+});
+
+app.listen(5000, () => console.log('SERVER STARTED...'));
